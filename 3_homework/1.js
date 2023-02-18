a785705d51467abbdaaae03d06048ef25e4c5cd7
Вероника Акулова <fenek1901@gmail.com>
2023-02-18 21:53:09 +0300

    Поправки 3 д/з

--- 3_homework/1.js
+++ 3_homework/1.js
@@ -1,4 +1,9 @@
+// const tempС = Number.parseFloat(prompt('Введите температуру в градусах Цельсия:'));
+// const tempF = ((9 / 5) * tempС + 32).toFixed(2); //toFixed - возвращает строку
+// alert(`Цельсий ${tempС}, Фаренгейт ${tempF}`);
+
 
 const tempС = Number.parseFloat(prompt('Введите температуру в градусах Цельсия:'));
-const tempF = ((9/5)*tempС + 32).toFixed(2);
-alert (`Цельсий ${tempС}, Фаренгейт ${tempF}`);
+const tempF = ((9 / 5) * tempС + 32);
+
+alert(`Цельсий ${tempС}, Фаренгейт ${Math.floor(tempF*100)/100}`);
